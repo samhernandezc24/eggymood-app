c9a89ad177c81345099e1f55e8311b63e44b90ea
samhernandezc24 <samd.hernandezc24@gmail.com>
2023-12-13 15:27:53 -0600

    Configure Loader

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:eggymood_app/core/screens/home_screen.dart';
+import 'package:eggymood_app/features/auth/presentation/screens/login_screen.dart';
+import 'package:eggymood_app/features/auth/presentation/screens/verify_email_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-import 'package:eggymood_app/features/auth/presentation/screens/verify_email_screen.dart';
+import 'package:eggymood_app/core/screens/loader.dart';
 import 'package:eggymood_app/config/routes/routes.dart';
 import 'package:eggymood_app/config/themes/app_theme.dart';
-import 'package:firebase_core/firebase_core.dart';
 
 import 'firebase_options.dart';
 
@@ -21,10 +25,29 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       theme: AppTheme.appTheme(),
+      debugShowCheckedModeBanner: false,
       title: 'EggyMood',
-      home: const VerifyEmailScreen(),
+      home: StreamBuilder(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Loader();
+          }
+
+          if (snapshot.hasData) {
+            final user = snapshot.data;
+
+            if (user!.emailVerified) {
+              return const HomeScreen();
+            } else {
+              return const VerifyEmailScreen();
+            }
+          }
+
+          return const LoginScreen();
+        },
+      ),
       onGenerateRoute: Routes.onGenerateRoute,
     );
   }
